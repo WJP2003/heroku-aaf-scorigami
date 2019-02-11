1f7495de89a30d6d5f50f0b76d05ebe23cf828b1
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2019-02-10 21:37:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,13 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
 	fs.readFile("board.html",function(err,data) {
-		if(!err) {
-			res.writeHead(200, {'Content-Type': ('text/html')});
-			res.write(data,function() {
-				res.end();
+		res.writeHead(200, {'Content-Type': ('text/html')});
+		res.write(data,function() {
+			fs.readFile("scores.txt",function(err,data2) {
+				res.write(data + data2);
 			});
-		} else {
-			res.writeHead(500, {'Content-Type': 'text/html'});
-			res.write("<html><body style='font-family:Verdana;font-size:5vw;font-weight:bold;'>500<br>Internal Server Error<br><br>" + err + "</body></html>", function() {
-				res.end();
-			});
-		}
+			res.end();
+		});
 	});
 }).listen(port);
 
